c2ffbd1a2b9f372184d47acd6519e825549684b0
danny bergh <dandeman111@gmail.com>
2016-09-08 15:35:18 +0200

    UseCase class gemaak met een constructor

---
+++ UseCaseHelper/UseCaseHelper/UseCase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCaseHelper
+{
+    class UseCase
+    {
+        public String Naam { get; set; }
+        public String Samenvatting { get; set; }
+        public List<Actor> Actors { get; set; }
+        public String Beschrijving { get; set; }
+        public String Uitzondering { get; set; }
+        public String Resultaat { get; set; }
+
+        public UseCase(String naam, String samenvatting, List<Actor> actors, String beschrijving, String uitzondering, String resultaat)
+        {
+            Naam = naam;
+            Samenvatting = samenvatting;
+            Actors = actors;
+            Beschrijving = beschrijving;
+            Uitzondering = uitzondering;
+            Resultaat = resultaat;
+        }
+    }
+}
