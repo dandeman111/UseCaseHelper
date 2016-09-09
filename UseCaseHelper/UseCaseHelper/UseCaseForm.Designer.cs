817ea6e6e592bdc8e02fdda7560cb2a01fd86274
danny bergh <dandeman111@gmail.com>
2016-09-09 10:39:30 +0200

    form gemaakt voor useCase

---
+++ UseCaseHelper/UseCaseHelper/UseCaseForm.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace UseCaseHelper
+{
+    partial class UseCaseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNaam = new System.Windows.Forms.Label();
+            this.lblSamenvatting = new System.Windows.Forms.Label();
+            this.lblActor = new System.Windows.Forms.Label();
+            this.lblAanname = new System.Windows.Forms.Label();
+            this.lblOmschrijving = new System.Windows.Forms.Label();
+            this.lblUitzondering = new System.Windows.Forms.Label();
+            this.lblResultaat = new System.Windows.Forms.Label();
+            this.tbNaam = new System.Windows.Forms.TextBox();
+            this.tbSamenvatting = new System.Windows.Forms.TextBox();
+            this.tbActor = new System.Windows.Forms.TextBox();
+            this.tbAanname = new System.Windows.Forms.TextBox();
+            this.tbOmschrijving = new System.Windows.Forms.TextBox();
+            this.tbUItzondering = new System.Windows.Forms.TextBox();
+            this.tbResultaat = new System.Windows.Forms.TextBox();
+            this.btnKlaar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNaam
+            // 
+            this.lblNaam.AutoSize = true;
+            this.lblNaam.Location = new System.Drawing.Point(31, 40);
+            this.lblNaam.Name = "lblNaam";
+            this.lblNaam.Size = new System.Drawing.Size(53, 17);
+            this.lblNaam.TabIndex = 0;
+            this.lblNaam.Text = "Naam :";
+            // 
+            // lblSamenvatting
+            // 
+            this.lblSamenvatting.AutoSize = true;
+            this.lblSamenvatting.Location = new System.Drawing.Point(31, 80);
+            this.lblSamenvatting.Name = "lblSamenvatting";
+            this.lblSamenvatting.Size = new System.Drawing.Size(102, 17);
+            this.lblSamenvatting.TabIndex = 1;
+            this.lblSamenvatting.Text = "Samenvatting :";
+            // 
+            // lblActor
+            // 
+            this.lblActor.AutoSize = true;
+            this.lblActor.Location = new System.Drawing.Point(31, 147);
+            this.lblActor.Name = "lblActor";
+            this.lblActor.Size = new System.Drawing.Size(49, 17);
+            this.lblActor.TabIndex = 2;
+            this.lblActor.Text = "Actor :";
+            // 
+            // lblAanname
+            // 
+            this.lblAanname.AutoSize = true;
+            this.lblAanname.Location = new System.Drawing.Point(31, 208);
+            this.lblAanname.Name = "lblAanname";
+            this.lblAanname.Size = new System.Drawing.Size(76, 17);
+            this.lblAanname.TabIndex = 3;
+            this.lblAanname.Text = "Aanname :";
+            // 
+            // lblOmschrijving
+            // 
+            this.lblOmschrijving.AutoSize = true;
+            this.lblOmschrijving.Location = new System.Drawing.Point(30, 272);
+            this.lblOmschrijving.Name = "lblOmschrijving";
+            this.lblOmschrijving.Size = new System.Drawing.Size(97, 17);
+            this.lblOmschrijving.TabIndex = 4;
+            this.lblOmschrijving.Text = "Omschrijving :";
+            // 
+            // lblUitzondering
+            // 
+            this.lblUitzondering.AutoSize = true;
+            this.lblUitzondering.Location = new System.Drawing.Point(30, 331);
+            this.lblUitzondering.Name = "lblUitzondering";
+            this.lblUitzondering.Size = new System.Drawing.Size(96, 17);
+            this.lblUitzondering.TabIndex = 5;
+            this.lblUitzondering.Text = "Uitzondering :";
+            // 
+            // lblResultaat
+            // 
+            this.lblResultaat.AutoSize = true;
+            this.lblResultaat.Location = new System.Drawing.Point(31, 396);
+            this.lblResultaat.Name = "lblResultaat";
+            this.lblResultaat.Size = new System.Drawing.Size(80, 17);
+            this.lblResultaat.TabIndex = 6;
+            this.lblResultaat.Text = "Resultaat : ";
+            // 
+            // tbNaam
+            // 
+            this.tbNaam.Location = new System.Drawing.Point(143, 40);
+            this.tbNaam.Name = "tbNaam";
+            this.tbNaam.Size = new System.Drawing.Size(185, 22);
+            this.tbNaam.TabIndex = 7;
+            // 
+            // tbSamenvatting
+            // 
+            this.tbSamenvatting.Location = new System.Drawing.Point(143, 80);
+            this.tbSamenvatting.Multiline = true;
+            this.tbSamenvatting.Name = "tbSamenvatting";
+            this.tbSamenvatting.Size = new System.Drawing.Size(348, 44);
+            this.tbSamenvatting.TabIndex = 8;
+            // 
+            // tbActor
+            // 
+            this.tbActor.Location = new System.Drawing.Point(143, 147);
+            this.tbActor.Multiline = true;
+            this.tbActor.Name = "tbActor";
+            this.tbActor.Size = new System.Drawing.Size(348, 44);
+            this.tbActor.TabIndex = 9;
+            // 
+            // tbAanname
+            // 
+            this.tbAanname.Location = new System.Drawing.Point(143, 208);
+            this.tbAanname.Multiline = true;
+            this.tbAanname.Name = "tbAanname";
+            this.tbAanname.Size = new System.Drawing.Size(348, 44);
+            this.tbAanname.TabIndex = 10;
+            // 
+            // tbOmschrijving
+            // 
+            this.tbOmschrijving.Location = new System.Drawing.Point(143, 269);
+            this.tbOmschrijving.Multiline = true;
+            this.tbOmschrijving.Name = "tbOmschrijving";
+            this.tbOmschrijving.Size = new System.Drawing.Size(348, 44);
+            this.tbOmschrijving.TabIndex = 11;
+            // 
+            // tbUItzondering
+            // 
+            this.tbUItzondering.Location = new System.Drawing.Point(143, 328);
+            this.tbUItzondering.Multiline = true;
+            this.tbUItzondering.Name = "tbUItzondering";
+            this.tbUItzondering.Size = new System.Drawing.Size(348, 44);
+            this.tbUItzondering.TabIndex = 12;
+            // 
+            // tbResultaat
+            // 
+            this.tbResultaat.Location = new System.Drawing.Point(143, 396);
+            this.tbResultaat.Multiline = true;
+            this.tbResultaat.Name = "tbResultaat";
+            this.tbResultaat.Size = new System.Drawing.Size(348, 44);
+            this.tbResultaat.TabIndex = 13;
+            // 
+            // btnKlaar
+            // 
+            this.btnKlaar.Location = new System.Drawing.Point(246, 462);
+            this.btnKlaar.Name = "btnKlaar";
+            this.btnKlaar.Size = new System.Drawing.Size(120, 32);
+            this.btnKlaar.TabIndex = 14;
+            this.btnKlaar.Text = "Klaar";
+            this.btnKlaar.UseVisualStyleBackColor = true;
+            // 
+            // UseCaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 517);
+            this.Controls.Add(this.btnKlaar);
+            this.Controls.Add(this.tbResultaat);
+            this.Controls.Add(this.tbUItzondering);
+            this.Controls.Add(this.tbOmschrijving);
+            this.Controls.Add(this.tbAanname);
+            this.Controls.Add(this.tbActor);
+            this.Controls.Add(this.tbSamenvatting);
+            this.Controls.Add(this.tbNaam);
+            this.Controls.Add(this.lblResultaat);
+            this.Controls.Add(this.lblUitzondering);
+            this.Controls.Add(this.lblOmschrijving);
+            this.Controls.Add(this.lblAanname);
+            this.Controls.Add(this.lblActor);
+            this.Controls.Add(this.lblSamenvatting);
+            this.Controls.Add(this.lblNaam);
+            this.Name = "UseCaseForm";
+            this.Text = "Vul gegevens van UseCase in";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNaam;
+        private System.Windows.Forms.Label lblSamenvatting;
+        private System.Windows.Forms.Label lblActor;
+        private System.Windows.Forms.Label lblAanname;
+        private System.Windows.Forms.Label lblOmschrijving;
+        private System.Windows.Forms.Label lblUitzondering;
+        private System.Windows.Forms.Label lblResultaat;
+        private System.Windows.Forms.TextBox tbNaam;
+        private System.Windows.Forms.TextBox tbSamenvatting;
+        private System.Windows.Forms.TextBox tbActor;
+        private System.Windows.Forms.TextBox tbAanname;
+        private System.Windows.Forms.TextBox tbOmschrijving;
+        private System.Windows.Forms.TextBox tbUItzondering;
+        private System.Windows.Forms.TextBox tbResultaat;
+        private System.Windows.Forms.Button btnKlaar;
+    }
+}
